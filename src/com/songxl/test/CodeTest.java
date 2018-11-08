1a44bb6cbf024e55ad8aae6f4135ed8d60ea126f
songxlLogin <42489609+songxlLogin@users.noreply.github.com>
2018-11-08 22:58:32 +0800

    Update CodeTest.java

--- src/com/songxl/test/CodeTest.java
+++ src/com/songxl/test/CodeTest.java
@@ -4,5 +4,6 @@ public class CodeTest {
 
     public static void main(String[] args) {
         System.out.println("zhangsan: this is test code");
+        System.out.println("lisi:this is update code");
     }
 }
